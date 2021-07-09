c25a896371bea761c04196ded7f077aedb342d80
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2021-07-09 10:01:20 -0400

    Airport DRF-2

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sky-view-drf-2",
+      "name": "Sky View DRF-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.020138414446089214,
+        "pitch": 0.48004192064311546,
+        "fov": 1.4236357722889479
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.015639763008950425,
+          "pitch": 1.0454805509723766,
+          "rotation": 0,
+          "target": "1-lower-view-drf-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lower-view-drf-2",
+      "name": "Lower View DRF-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.002685121926162992,
+        "pitch": 0.07512106391743067,
+        "fov": 1.4236357722889479
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03990359185500125,
+          "pitch": 0.49133595301368516,
+          "rotation": 0,
+          "target": "0-sky-view-drf-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Durable Rock Fill 2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
